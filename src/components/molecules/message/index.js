0655f8f5664c60c0de5197b5b8650a6477fc1e74
coreybyrum7 <coreybyrum7@gmail.com>
2022-05-17 12:44:53 -0400

    Minor updates - Renders more prior to last message being shown

--- src/components/molecules/message/index.js
+++ src/components/molecules/message/index.js
@@ -3,37 +3,39 @@ import Moment from 'react-moment';
 import ClampLines from 'react-clamp-lines';
 import { messages_api } from '../../../data';
 
-export const Message = ({ message, isLast} ) => {
+export const Message = ({ index, message, isLast} ) => {
     const { content, updated, author } = message;
     const { name, photoUrl } = author;
 
     return (
-        <article 
+        <article
             key={message.id}
-            className={isLast ? 'message--last' : 'message'}
+            className={isLast ? 'message--last' : 'message__container'}
         >
-            <div className="message__head">
-                <div className="message__avatar">
-                    <img 
-                        src={`${messages_api}/${photoUrl}`} 
-                        alt="image" 
-                        width="45" 
-                        height="45"
+            <div className="message">
+                <div className="message__head">
+                    <div className="message__avatar">
+                        <img 
+                            src={`${messages_api}/${photoUrl}`} 
+                            alt="image" 
+                            width="45" 
+                            height="45"
+                        />
+                    </div>
+                    <div className="message__info">
+                        <h4>{name}</h4>
+                        <p className="paragraph--subtext">
+                            <Moment fromNow>{updated}</Moment>
+                        </p>
+                    </div>
+                </div>
+                <div className="message__body">
+                    <ClampLines
+                        text={content}
+                        lines={4}
+                        buttons={false}
                     />
                 </div>
-                <div className="message__info">
-                    <h4>{name}</h4>
-                    <p className="paragraph--subtext">
-                        <Moment fromNow>{updated}</Moment>
-                    </p>
-                </div>
-            </div>
-            <div className="message__body">
-                <ClampLines
-                    text={content}
-                    lines={4}
-                    buttons={false}
-                />
             </div>
         </article>
     )
--- src/components/organisms/feed/index.js
+++ src/components/organisms/feed/index.js
@@ -2,38 +2,39 @@ import './index.scss';
 import { messages_api } from '../../../data';
 import { Message } from '../../../components';
 import { InView } from 'react-intersection-observer';
+import { useSprings, animated, interpolate } from 'react-spring'
+import { useGesture } from 'react-use-gesture';
 import { useEffect, useState } from 'react';
 
 export const Feed = () => {
-    const [ isFetching, setIsFetching ] = useState(false);
+    const [ isFetched, setIsFetched ] = useState(false);
     const [ token, setToken ] = useState('');
     const [ feed, setFeed ] = useState([]);
+
+    useEffect(() => {
+        loadMore();
+    }, []);
+
+    const parseData = (data) => {
+        setToken(data.pageToken)
+        setFeed(feed.concat(data.messages))
+        setIsFetched(true)
+    }
     
     const loadMore = (pageToken) => {
         fetch(`${messages_api}/messages?limit=20&pageToken=${pageToken}`)
             .then(response => response.json())
             .then(data => {
                 parseData(data)
-                setIsFetching(false)
             })
             .catch(e => {
                 console.log(e);
-            })
+            });
     }
-
-    const parseData = (data) => {
-        setToken(data.pageToken)
-        setFeed(feed.concat(data.messages))
-    }
-
-    useEffect(() => {
-        setIsFetching(true);
-        loadMore();
-    }, []);
 
     return (
         <div className="feed">
-            {!isFetching && feed.map((feedUnit, index) => (
+            {isFetched && feed.map((feedUnit, index) => (
                 (index !== feed.length - 1) ?
                     <Message
                         message={feedUnit}
